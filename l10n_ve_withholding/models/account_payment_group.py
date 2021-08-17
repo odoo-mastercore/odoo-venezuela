46ee99e7cbc02856095ab6bf77a3514208667e81
rhe-mastercore <rhe@sinapsys.global>
2021-08-17 08:02:14 -0400

    [ADD] foreign currency amounts in payment

--- l10n_ve_withholding/models/account_payment_group.py
+++ l10n_ve_withholding/models/account_payment_group.py
@@ -31,7 +31,17 @@ class AccountPaymentGroup(models.Model):
         'seniat.tabla.islr',
         compute='_partner_regimenes_islr',
     )
-
+    #This field is to be used by invoice in multicurrency
+    selected_finacial_debt_currency = fields.Monetary(
+        string='Selected Financial Debt in foreign currency',
+        compute='_compute_selected_debt',
+    )
+    debt_multicurrency = fields.Boolean(
+        string='debt is in foreign currency?', default=False,
+    )
+    selected_debt_currency_id = fields.Many2one("res.currency",
+        string='Selected Debt in foreign currency',
+    )
     @api.depends('partner_id.seniat_regimen_islr_ids')
     def _partner_regimenes_islr(self):
         """
@@ -59,19 +69,31 @@ class AccountPaymentGroup(models.Model):
             selected_debt = 0.0
             selected_debt_untaxed = 0.0
             selected_debt_taxed = 0.0
+            selected_finacial_debt_currency = 0.0
             for line in rec.to_pay_move_line_ids:
                 #this is conditional used to vat retention
                 for abg in line.move_id.amount_by_group:
                     if str(abg[0]).find('IVA') > -1:
                         selected_debt_taxed += abg[1]
                 selected_finacial_debt += line.financial_amount_residual
-                selected_debt += line.move_id.amount_residual
                 # factor for total_untaxed
                 invoice = line.move_id
+                if line.move_id.currency_id != rec.company_id.currency_id:
+                    selected_finacial_debt_currency += line.amount_residual_currency
+                    rec.debt_multicurrency = True
+                    rec.selected_debt_currency_id = line.move_id.currency_id.id
+                elif line.move_id.currency_id != rec.company_id.currency_id and rec.debt_multicurrency:
+                    selected_finacial_debt_currency += line.amount_residual_currency
+                    rec.debt_multicurrency = True
+                else:
+                    rec.debt_multicurrency = False
+                    #selected_debt += line.move_id.amount_residual
+                selected_debt += line.amount_residual
                 factor = invoice and invoice._get_tax_factor() or 1.0
                 selected_debt_untaxed += line.amount_residual * factor
             sign = 1.0
             rec.selected_finacial_debt = selected_finacial_debt * sign
             rec.selected_debt = selected_debt * sign
+            rec.selected_finacial_debt_currency = selected_finacial_debt_currency * sign
             rec.selected_debt_untaxed = selected_debt_untaxed * sign
             rec.selected_debt_taxed = selected_debt_taxed
