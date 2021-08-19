7a9deb3ca60724cc450ec8d87b63e9d60bb33046
rhe-mastercore <rhe@sinapsys.global>
2021-08-19 13:19:11 -0400

    [ADD] amount in company currency for a single tax in invoice report

--- l10n_ve_invoice_reports/models/account_move.py
+++ l10n_ve_invoice_reports/models/account_move.py
@@ -14,15 +14,16 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def invoice_rate(self, currency_id, invoice_date):
-        last_rate = self.env['res.currency.rate'].search([
-            ('currency_id', '=', currency_id),
-            ('name', '=', invoice_date)
-        ],limit=1).rate
-        if not last_rate:
-            last_rate = 1
-        return last_rate
+        for rec in self:
+            last_rate = self.env['res.currency.rate'].search([
+                ('currency_id', '=', currency_id),
+                ('name', '=', invoice_date)
+            ],limit=1).rate
+            if not last_rate:
+                last_rate = 1
+            return last_rate
 
-    def amount_in_company_currency(self, amount, currency_id, date):
+    def amount_str_in_company_currency(self, amount, currency_id, date):
         for rec in self:
             result = 0.00
             if currency_id and date:
