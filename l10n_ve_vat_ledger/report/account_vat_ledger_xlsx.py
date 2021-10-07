38d01498dc11500ec89ed3d5efc802f2685f8918
rhe-mastercore <rhe@sinapsys.global>
2021-10-07 14:47:39 -0400

    [ADD] including default values in the VAT book and invoices without VAT to sales

--- l10n_ve_vat_ledger/report/account_vat_ledger_xlsx.py
+++ l10n_ve_vat_ledger/report/account_vat_ledger_xlsx.py
@@ -66,8 +66,8 @@ class AccountVatLedgerXlsx(models.AbstractModel):
                     sheet.write(row, 3, invoice.l10n_ve_document_number or 'FALSE', line)
                     sheet.write(row, 4, invoice.partner_id.name or 'FALSE', line)
                     sheet.write(row, 5,  '%s-%s' %(invoice.partner_id.\
-                    l10n_latam_identification_type_id.l10n_ve_code or 'FALSE',
-                    invoice.partner_id.vat or 'FALSE'), line)
+                        l10n_latam_identification_type_id.l10n_ve_code or 'FALSE',
+                        invoice.partner_id.vat or 'FALSE'), line)
                     sheet.write(row, 6, '', line)
                     sheet.write(row, 7, '', line)
                     sheet.write(row, 8, '', line)
@@ -103,36 +103,30 @@ class AccountVatLedgerXlsx(models.AbstractModel):
                     # Write Sale lines
                     i += 1
                     sheet.write(row, 0, i, line)
-                    sheet.write(row, 1, invoice.invoice_date, date_line)
-                    sheet.write(row, 2, invoice.name, line)
+                    sheet.write(row, 1, invoice.invoice_date or 'FALSE', date_line)
+                    sheet.write(row, 2, invoice.name or 'FALSE', line)
                     sheet.write(row, 3, invoice.l10n_ve_document_number or 'FALSE', line)
                     sheet.write(row, 4, invoice.partner_id.name or 'FALSE', line)
-                    if invoice.partner_id.\
-                        l10n_latam_identification_type_id.l10n_ve_code and invoice.\
-                            partner_id.vat:
-                        sheet.write(row, 5, invoice.partner_id.\
-                            l10n_latam_identification_type_id.l10n_ve_code+'-'+invoice.\
-                            partner_id.vat, line)
-                    elif invoice.partner_id.\
-                        l10n_latam_identification_type_id.l10n_ve_code:
-                        sheet.write(row, 5, invoice.partner_id.\
-                            l10n_latam_identification_type_id.l10n_ve_code+'-'+'FALSE', line)
-                    else:
-                        sheet.write(row, 5, 'FALSE'+'-'+invoice.partner_id.vat, line)
+                    sheet.write(row, 5,  '%s-%s' %(invoice.partner_id.\
+                        l10n_latam_identification_type_id.l10n_ve_code or 'FALSE',
+                        invoice.partner_id.vat or 'FALSE'), line)
                     sheet.write(row, 6, '', line)
                     sheet.write(row, 7, '', line)
                     sheet.write(row, 8, '', line)
                     sheet.write(row, 9, '01-REG', line)
-                    sheet.write(row, 10, int((str(
-                        invoice.amount_by_group[0][0]).replace("IVA ","")).\
-                        replace("%","")), line)
-                    sheet.write(row, 11, invoice.amount_untaxed_signed, line)
+                    
                     if invoice.amount_tax_signed == 0:
+                        sheet.write(row, 10, 0, line)
+                        sheet.write(row, 11, 0, line)
                         sheet.write(row, 12, invoice.amount_tax_signed, line)
                         sheet.write(row, 13, 0, line)
                         sheet.write(row, 14, invoice.amount_total_signed, line)
                         total_amount_other_tax += invoice.amount_total_signed
                     else:
+                        sheet.write(row, 10, int((str(
+                            invoice.amount_by_group[0][0]).replace("IVA ","")).\
+                            replace("%","")), line)
+                        sheet.write(row, 11, invoice.amount_untaxed_signed, line)
                         sheet.write(row, 12, invoice.amount_tax_signed, line)
                         sheet.write(row, 13, invoice.amount_total_signed, line)
                         sheet.write(row, 14, 0, line)
