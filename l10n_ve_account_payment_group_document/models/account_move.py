91ac500403b89c4637dea669ee1cd4e915e98fda
rhe-mastercore <rhe@sinapsys.global>
2022-02-10 09:10:15 -0400

    [FIX] remove _check_unique_sequence_number for account payment group

--- l10n_ve_account_payment_group_document/models/account_move.py
+++ l10n_ve_account_payment_group_document/models/account_move.py
@@ -2,7 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, api
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = "account.move"
@@ -10,5 +12,5 @@ class AccountMove(models.Model):
     @api.constrains('name', 'journal_id', 'state')
     def _check_unique_sequence_number(self):
         payment_group_moves = self.filtered(
-            lambda x: x.journal_id.type in ['cash', 'bank'] and x.l10n_latam_document_type_id)
+            lambda x: x.journal_id.type in ['cash', 'bank'] and x.payment_id.payment_group_id)
         return super(AccountMove, self - payment_group_moves)._check_unique_sequence_number()
