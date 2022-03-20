5b95521af040a3865f93614b844a07cdc70ce5f1
Kewitz Colina <kewitz.colina@gmail.com>
2022-03-19 20:22:27 -0400

    [WIP] Módulo l10n_ve_invoice_report

---
+++ l10n_ve_invoice_reports/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# Author      : SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyright(c): 2021-Present.
+# License URL : AGPL-3
+################################################################################
+
+from . import models
