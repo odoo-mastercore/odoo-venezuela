73a9d70a5c0df1d102be8d892285efa24e4aab99
rhe-mastercore <rhe@sinapsys.global>
2022-05-18 00:15:57 -0400

    [ADD] assign control number to corrective invoice

--- l10n_ve_withholding/wizard/account_move_reverse.py
+++ l10n_ve_withholding/wizard/account_move_reverse.py
@@ -16,7 +16,7 @@ class AccountMoveReversal(models.TransientModel):
 
     def _prepare_default_reversal(self, move):
         return {
-            'ref': _('Reversal of: %s, %s') % (move.name, self.reason) if self.reason else _('Reversal of: %s') % (move.name),
+            'ref': _('Reversión de: %s, %s') % (move.name, self.reason) if self.reason else _('Reversión de: %s') % (move.name),
             'date': self.date or move.date,
             'invoice_date': move.is_invoice(include_receipts=True) and (self.date or move.date) or False,
             'journal_id': self.journal_id and self.journal_id.id or move.journal_id.id,
@@ -27,9 +27,11 @@ class AccountMoveReversal(models.TransientModel):
         }
 
 
+    #TODO: ver si esto es necesario.
     def reverse_moves(self):
-        """ Forzamos fecha de la factura original para que el amount total de la linea se calcule bien"""
+        """ Forzamos el seteo limpio"""
         res = super(AccountMoveReversal, self).reverse_moves()
+        #Nunca esta pasando por aqui.
         for rec in self:
             self.move_ids.l10n_ve_document_number = ""
         return res
