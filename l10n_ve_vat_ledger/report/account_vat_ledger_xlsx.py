cfb2e348cc02edc2310b9a75e582d8b99dd4eb7c
ejpr4 <elvispaez18@gmail.com>
2023-02-09 13:20:06 -0400

    [REF] sales book

--- l10n_ve_vat_ledger/report/account_vat_ledger_xlsx.py
+++ l10n_ve_vat_ledger/report/account_vat_ledger_xlsx.py
@@ -759,11 +759,11 @@ class AccountVatLedgerXlsx(models.AbstractModel):
                                     if invoice.move_type == 'out_refund' or \
                                         invoice.move_type == 'in_refund' or (invoice.move_type == 'out_invoice' \
                                             and invoice.debit_origin_id):
-                                        base_imponible = base_imponible * -1.00
+                                        base_imponible = linel.debit * -1.00
                                         if not invoice.debit_origin_id:
                                             total_nota_credito_16 += base_imponible
                                         else:
-                                            base_imponible = base_imponible * -1.00
+                                            base_imponible = linel.credit
                                             total_nota_debito_16 += base_imponible
                                     else:
                                         total_base_imponible_16 += base_imponible
@@ -772,11 +772,11 @@ class AccountVatLedgerXlsx(models.AbstractModel):
                                     base_exento += linel.credit
                                     if invoice.move_type == 'out_refund' or invoice.move_type == 'in_refund' \
                                         or (invoice.move_type == 'out_invoice' and invoice.debit_origin_id):
-                                        base_exento = base_exento * -1.00
-                                        if invoice.debit_origin_id:
-                                            base_exento = base_exento * -1.00
+                                        base_exento = linel.debit * -1.00
+                                        if not invoice.debit_origin_id:
                                             total_base_exento_debito += base_exento
                                         else:
+                                            base_exento = linel.credit
                                             total_base_exento_credito += base_exento
                                     else:
                                         total_base_exento += base_exento
@@ -784,11 +784,11 @@ class AccountVatLedgerXlsx(models.AbstractModel):
                                     base_imponible_8 += linel.credit
                                     if invoice.move_type == 'out_refund' or invoice.move_type == 'in_refund' \
                                         or (invoice.move_type == 'out_invoice' and invoice.debit_origin_id):
-                                        base_imponible_8 = base_imponible_8 * -1.00
+                                        base_imponible_8 = linel.debit * -1.00
                                         if not invoice.debit_origin_id:
                                             total_nota_credito_8 += base_imponible_8
                                         else:
-                                            base_imponible_8 = base_imponible_8 * -1.00
+                                            base_imponible_8 = linel.credit
                                             total_nota_debito_8 = base_imponible_8
                                     else:
                                         total_base_imponible_8 += base_imponible_8
@@ -798,11 +798,11 @@ class AccountVatLedgerXlsx(models.AbstractModel):
                                 if invoice.move_type == 'out_refund' or \
                                         invoice.move_type == 'in_refund' or (invoice.move_type == 'out_invoice' \
                                             and invoice.debit_origin_id):
-                                    iva_16 = iva_16 * -1.00
+                                    iva_16 = linel.debit * -1.00
                                     if not invoice.debit_origin_id:
                                         total_nota_credito_iva_16 += iva_16
                                     else:
-                                        iva_16 = iva_16 * -1.00
+                                        iva_16 = linel.credit
                                         total_nota_debito_iva_16 += iva_16
                                 else:
                                     total_iva_16 += iva_16
@@ -810,11 +810,11 @@ class AccountVatLedgerXlsx(models.AbstractModel):
                                 iva_8 += linel.credit
                                 if invoice.move_type == 'out_refund' or invoice.move_type == 'in_refund' \
                                         or (invoice.move_type == 'out_invoice' and invoice.debit_origin_id):
-                                    iva_8 = iva_8 * -1.00
+                                    iva_8 = linel.debit * -1.00
                                     if not invoice.debit_origin_id:
                                         total_nota_credito_iva_8 += iva_8
                                     else:
-                                        iva_8 = iva_8 * -1.00
+                                        iva_8 = linel.credit
                                         total_nota_debito_iva_8 += iva_8
                                 else:
                                     total_iva_8 += iva_8
