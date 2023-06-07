d24c7967b42c6ccdf4011328453558b3f7a8cf49
Elvis Paez <elvispaez18@gmail.com>
2023-06-07 16:40:00 -0400

    [FIX] account move reverse

--- l10n_ve_withholding/wizard/account_move_reverse.py
+++ l10n_ve_withholding/wizard/account_move_reverse.py
@@ -15,14 +15,18 @@ class AccountMoveReversal(models.TransientModel):
 
 
     def _prepare_default_reversal(self, move):
+        reverse_date = self.date if self.date_mode == 'custom' else move.date        
         return {
-            'ref': _('Reversión de: %s, %s') % (move.name, self.reason) if self.reason else _('Reversión de: %s') % (move.name),
-            'date': self.date or move.date,
+            'ref': _('Reversal of: %(move_name)s, %(reason)s', move_name=move.name, reason=self.reason)
+                   if self.reason
+                   else _('Reversal of: %s', move.name),
+            'date': reverse_date,
+            'invoice_date_due': reverse_date,
             'invoice_date': move.is_invoice(include_receipts=True) and (self.date or move.date) or False,
-            'journal_id': self.journal_id and self.journal_id.id or move.journal_id.id,
+            'journal_id': self.journal_id.id,
             'invoice_payment_term_id': None,
-            'auto_post': True if self.date > fields.Date.context_today(self) else False,
             'invoice_user_id': move.invoice_user_id.id,
+            'auto_post': 'at_date' if reverse_date > fields.Date.context_today(self) else 'no',
             'l10n_ve_document_number': ""
         }
 
