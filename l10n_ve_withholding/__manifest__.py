b4783a1503348843a8ff0831f09abf96230339d5
Reydi Hernandez <rhe@sinapsys.global>
2023-11-21 07:35:25 -0400

    [ADD]Multitax for islr retention

--- l10n_ve_withholding/__manifest__.py
+++ l10n_ve_withholding/__manifest__.py
@@ -17,7 +17,7 @@
 
     'author': "SINAPSYS GLOBAL SA || MASTERCORE SAS",
     'website': "http://sinapsys.global",
-    'version': '16.0.0.1.3',
+    'version': '16.0.0.1.4',
     'category': 'Localization',
     'license': 'AGPL-3',
     'depends': [
--- l10n_ve_withholding/models/account_tax.py
+++ l10n_ve_withholding/models/account_tax.py
@@ -86,24 +86,38 @@ class AccountTax(models.Model):
 
         elif self.withholding_type == 'tabla_islr':
             regimen = payment_group.regimen_islr_id
+            ctx = self._context.copy()
+            default_regimen_islr_id = ctx.get('default_regimen_islr_id', None)
+            if default_regimen_islr_id:
+                regimen =  self.env['seniat.tabla.islr'].search([
+                    ('id', '=', default_regimen_islr_id),], limit=1)
+            else:
+                regimen = payment_group.regimen_islr_id
             vals = super(AccountTax, self).get_withholding_vals(
-                payment_group, force_withholding_amount_type)
-
+                payment_group)
+            default_regimen_islr_id = ctx.get('default_regimen_islr_id', None)
             to_pay = payment_group.to_pay_move_line_ids[0]
             selected_debt_untaxed = (to_pay.move_id.amount_untaxed_signed * -1.00)
-            if to_pay:
-                product_off = []
-                amount_off = 0.00
-                if to_pay.move_id.line_ids:
-                    for li in to_pay.move_id.invoice_line_ids:
-                        if li.product_id.product_tmpl_id.disable_islr:
-                            product_off.append(li.name)
-                    if product_off:
-                        for abg in to_pay.move_id.line_ids:
-                            if abg.name in product_off:
-                                amount_off += abg.debit
-                        selected_debt_untaxed = (
-                            to_pay.move_id.amount_untaxed_signed * -1.00) - amount_off
+            if default_regimen_islr_id:
+                lines_base = 0
+                for line in payment_group.withholding_distributin_islr_ids:
+                    if line.regimen_islr_id == regimen:
+                        lines_base += line.price_subtotal
+                selected_debt_untaxed = lines_base
+            else:
+                if to_pay:
+                    product_off = []
+                    amount_off = 0.00
+                    if to_pay.move_id.line_ids:
+                        for li in to_pay.move_id.invoice_line_ids:
+                            if li.product_id.product_tmpl_id.disable_islr:
+                                product_off.append(li.name)
+                        if product_off:
+                            for abg in to_pay.move_id.line_ids:
+                                if abg.name in product_off:
+                                    amount_off += abg.debit
+                            selected_debt_untaxed = (
+                                to_pay.move_id.amount_untaxed_signed * -1.00) - amount_off
             base = selected_debt_untaxed
             base_withholding = base * (
                 regimen.withholding_base_percentage / 100)
@@ -136,6 +150,8 @@ class AccountTax(models.Model):
             else:
                 withholding = base_withholding * withholding_percentage
 
+            vals['concept_withholding'] = str(regimen.code_seniat)+' -
+             '+str(regimen.activity_name)
             vals['comment_withholding'] = str(withholding_percentage*100)+"%"
             vals['total_amount'] = base
             vals['withholdable_invoiced_amount'] = base
@@ -187,74 +203,143 @@ class AccountTax(models.Model):
                 domain.append(('id', '=', payment_group.id))
                 if payment_group.search(domain):
                     raise ValidationError(tax.withholding_user_error_message)
-            vals = tax.get_withholding_vals(payment_group)
-
-            # we set computed_withholding_amount, hacemos round porque
-            # si no puede pasarse un valor con mas decimales del que se ve
-            # y terminar dando error en el asiento por debitos y creditos no
-            # son iguales, algo parecido hace odoo en el compute_all de taxes
-            currency = payment_group.currency_id
-            period_withholding_amount = currency.round(vals.get(
-                'period_withholding_amount', 0.0))
-            previous_withholding_amount = currency.round(vals.get(
-                'previous_withholding_amount'))
-            # withholding can not be negative
-            computed_withholding_amount = max(0, (
-                period_withholding_amount - previous_withholding_amount))
-
-            if not computed_withholding_amount:
-                # if on refresh no more withholding, we delete if it exists
+            if payment_group.withholding_distributin_islr_ids:
                 if payment_withholding:
-                    payment_withholding.unlink()
-                continue
+                    payment_withholdings = self.env[
+                    'account.payment'].search([
+                        ('payment_group_id', '=', payment_group.id),
+                        ('tax_withholding_id.withholding_type', '=', 'tabla_islr'),
+                        ('automatic', '=', True),])
+                    for p in payment_withholdings:
+                        p.unlink()
+                for islr in payment_group.withholding_distributin_islr_ids.mapped('regimen_islr_id'):
+                    vals = tax.with_context(default_regimen_islr_id = islr.id).get_withholding_vals(payment_group)
+                    currency = payment_group.currency_id
+                    period_withholding_amount = currency.round(vals.get(
+                        'period_withholding_amount', 0.0))
+                    previous_withholding_amount = currency.round(vals.get(
+                        'previous_withholding_amount'))
+                    # withholding can not be negative
+                    computed_withholding_amount = max(0, (
+                        period_withholding_amount - previous_withholding_amount))
 
-            # we copy withholdable_base_amount on base_amount
-            # al final vimos con varios clientes que este monto base
-            # debe ser la base imponible de lo que se está pagando en este
-            # voucher
-            vals['withholding_base_amount'] = vals.get(
-                'withholdable_advanced_amount') + vals.get(
-                'withholdable_invoiced_amount')
-            if vals.get('currency_id') == payment_group.company_id.currency_id.id:
-                vals['amount'] = computed_withholding_amount
-            vals['computed_withholding_amount'] = computed_withholding_amount
+                    if not computed_withholding_amount:
+                        # if on refresh no more withholding, we delete if it exists
+                        #if payment_withholding:
+                        #    payment_withholding.unlink()
+                        continue
 
-            # por ahora no imprimimos el comment, podemos ver de llevarlo a
-            # otro campo si es de utilidad
-            vals.pop('comment')
-            if payment_withholding:
-                payment_withholding.write(vals)
+                    # we copy withholdable_base_amount on base_amount
+                    # al final vimos con varios clientes que este monto base
+                    # debe ser la base imponible de lo que se está pagando en este
+                    # voucher
+                    vals['withholding_base_amount'] = vals.get(
+                        'withholdable_advanced_amount') + vals.get(
+                        'withholdable_invoiced_amount')
+                    if vals.get('currency_id') == payment_group.company_id.currency_id.id:
+                        vals['amount'] = computed_withholding_amount
+                    vals['computed_withholding_amount'] = computed_withholding_amount
+
+                    # por ahora no imprimimos el comment, podemos ver de llevarlo a
+                    # otro campo si es de utilidad
+                    vals.pop('comment')
+                    #if payment_withholding:
+                        #payment_withholding.write(vals)
+                        #pass
+                    #else:
+                    payment_method = self.env.ref(
+                        'account_withholding.'
+                        'account_payment_method_out_withholding')
+                    if tax.withholding_type == 'tabla_islr':
+                        journal = self.env['account.journal'].search([
+                            ('company_id', '=', tax.company_id.id),
+                            ('outbound_payment_method_line_ids.payment_method_id','=', payment_method.id),
+                            ('type', 'in', ['cash', 'bank']),
+                            ('apply_islr', '=', True),
+                        ], limit=1)
+                    if not journal:
+                        raise UserError(_(
+                            'No journal for withholdings found on company %s') % (
+                            tax.company_id.name))
+
+                    method = journal._get_available_payment_method_lines('outbound').filtered(
+                        lambda x: x.code == 'withholding')
+
+                    vals['journal_id'] = journal.id
+                    vals['payment_method_line_id'] = method.id
+                    vals['payment_type'] = 'outbound'
+                    vals['partner_type'] = payment_group.partner_type
+                    vals['partner_id'] = payment_group.partner_id.id
+                    payment_withholding = payment_withholding.create(vals)
+
             else:
-                payment_method = self.env.ref(
-                    'account_withholding.'
-                    'account_payment_method_out_withholding')
-                if tax.withholding_type == 'partner_tax':
-                    journal = self.env['account.journal'].search([
-                        ('company_id', '=', tax.company_id.id),
-                        ('outbound_payment_method_line_ids.payment_method_id',
-                         '=', payment_method.id),
-                        ('type', 'in', ['cash', 'bank']),
-                        ('apply_iva', '=', True),
-                    ], limit=1)
-                if tax.withholding_type == 'tabla_islr':
-                    journal = self.env['account.journal'].search([
-                        ('company_id', '=', tax.company_id.id),
-                        ('outbound_payment_method_line_ids.payment_method_id','=', payment_method.id),
-                        ('type', 'in', ['cash', 'bank']),
-                        ('apply_islr', '=', True),
-                    ], limit=1)
-                if not journal:
-                    raise UserError(_(
-                        'No journal for withholdings found on company %s') % (
-                        tax.company_id.name))
+                vals = tax.get_withholding_vals(payment_group)
+                # we set computed_withholding_amount, hacemos round porque
+                # si no puede pasarse un valor con mas decimales del que se ve
+                # y terminar dando error en el asiento por debitos y creditos no
+                # son iguales, algo parecido hace odoo en el compute_all de taxes
+                currency = payment_group.currency_id
+                period_withholding_amount = currency.round(vals.get(
+                    'period_withholding_amount', 0.0))
+                previous_withholding_amount = currency.round(vals.get(
+                    'previous_withholding_amount'))
+                # withholding can not be negative
+                computed_withholding_amount = max(0, (
+                    period_withholding_amount - previous_withholding_amount))
 
-                method = journal._get_available_payment_method_lines('outbound').filtered(
-                    lambda x: x.code == 'withholding')
+                if not computed_withholding_amount:
+                    # if on refresh no more withholding, we delete if it exists
+                    if payment_withholding:
+                        payment_withholding.unlink()
+                    continue
 
-                vals['journal_id'] = journal.id
-                vals['payment_method_line_id'] = method.id
-                vals['payment_type'] = 'outbound'
-                vals['partner_type'] = payment_group.partner_type
-                vals['partner_id'] = payment_group.partner_id.id
-                payment_withholding = payment_withholding.create(vals)
+                # we copy withholdable_base_amount on base_amount
+                # al final vimos con varios clientes que este monto base
+                # debe ser la base imponible de lo que se está pagando en este
+                # voucher
+                vals['withholding_base_amount'] = vals.get(
+                    'withholdable_advanced_amount') + vals.get(
+                    'withholdable_invoiced_amount')
+                if vals.get('currency_id') == payment_group.company_id.currency_id.id:
+                    vals['amount'] = computed_withholding_amount
+                vals['computed_withholding_amount'] = computed_withholding_amount
+
+                # por ahora no imprimimos el comment, podemos ver de llevarlo a
+                # otro campo si es de utilidad
+                vals.pop('comment')
+                if payment_withholding:
+                    payment_withholding.write(vals)
+                else:
+                    payment_method = self.env.ref(
+                        'account_withholding.'
+                        'account_payment_method_out_withholding')
+                    if tax.withholding_type == 'partner_tax':
+                        journal = self.env['account.journal'].search([
+                            ('company_id', '=', tax.company_id.id),
+                            ('outbound_payment_method_line_ids.payment_method_id',
+                            '=', payment_method.id),
+                            ('type', 'in', ['cash', 'bank']),
+                            ('apply_iva', '=', True),
+                        ], limit=1)
+                    if tax.withholding_type == 'tabla_islr':
+                        journal = self.env['account.journal'].search([
+                            ('company_id', '=', tax.company_id.id),
+                            ('outbound_payment_method_line_ids.payment_method_id','=', payment_method.id),
+                            ('type', 'in', ['cash', 'bank']),
+                            ('apply_islr', '=', True),
+                        ], limit=1)
+                    if not journal:
+                        raise UserError(_(
+                            'No journal for withholdings found on company %s') % (
+                            tax.company_id.name))
+
+                    method = journal._get_available_payment_method_lines('outbound').filtered(
+                        lambda x: x.code == 'withholding')
+
+                    vals['journal_id'] = journal.id
+                    vals['payment_method_line_id'] = method.id
+                    vals['payment_type'] = 'outbound'
+                    vals['partner_type'] = payment_group.partner_type
+                    vals['partner_id'] = payment_group.partner_id.id
+                    payment_withholding = payment_withholding.create(vals)
         return True
