5ace52222a367675693e80a7973937d6f11135af
jpi-mastercore <jpi@sinapsys.global>
2024-01-19 14:40:25 -0400

    [ADD] unique_vat_by_partner

--- unique_vat_by_partner/models/univat.py
+++ unique_vat_by_partner/models/univat.py
@@ -10,7 +10,6 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 import logging
-# _logger = logging.getLogger(__name__)
 
 class UniVat(models.Model):
     _inherit = 'res.partner'
