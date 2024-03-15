0394ac2116a58296372f6d86276867ecdd790d6a
Elvis Paez <elvispaez18@gmail.com>
2024-03-15 15:44:48 -0400

    [FIX] withholding distribution amount

--- l10n_ve_withholding/models/account_tax.py
+++ l10n_ve_withholding/models/account_tax.py
@@ -56,14 +56,14 @@ class AccountTax(models.Model):
                 if to_pay.move_id.line_ids:
                     for abg in to_pay.move_id.line_ids:
                         if abg.name in taxes:
-                            withholdable_invoiced_amount += abg.tax_base_amount
-                            invoice_amount = abg.tax_base_amount
+                            
                             tax_amount = abg.debit
                             alic = alicuota
                             withholding_amount = abg.debit*alicuota
+                            invoice_amount = 0.00
                             for abg_base in to_pay.move_id.line_ids.filtered(lambda x: x.tax_ids.name in [abg.name]):
-                                withholdable_invoiced_amount += abg_base.debit if to_pay.move_id.move_type == 'in_refund' else abg_base.credit
-                                invoice_amount += abg_base.debit if to_pay.move_id.move_type == 'in_refund' else abg_base.credit
+                                withholdable_invoiced_amount += abg_base.debit if to_pay.move_id.move_type != 'in_refund' else abg_base.credit
+                                invoice_amount += abg_base.debit if to_pay.move_id.move_type != 'in_refund' else abg_base.credit
                             if foreign_currency:
                                 selected_debt_taxed += abg.amount_currency if abg.amount_currency  >= 0 else -abg.amount_currency
                             else:
