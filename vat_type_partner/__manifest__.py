e87020bbabc7894b2fefa3152b217e00e406e891
Elvis Paez <elvispaez18@gmail.com>
2024-03-21 12:25:06 -0400

    [ADD] 19575

---
+++ vat_type_partner/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "VAT type by Partner",
+    'description': """
+
+        Este es el módulo vat_type_partner para la
+        implementación de los tipos de identificación
+        por tipo de contacto
+
+    """,
+
+    'author': "SINAPSYS GLOBAL SA || MASTERCORE SAS",
+    'website': "http://sinapsys.global",
+    'version': '15.0.1',
+    'category': 'Localization',
+    'license': 'AGPL-3',
+    'depends': ['base', 'contacts', 'l10n_ve_base'],
+}
---
+++ vat_type_partner/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2024-Present.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+
+from . import res_partner
