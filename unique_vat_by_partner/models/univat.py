92f2fd26d59b18bd59425a91f57599e1ceb19754
Rouse Milano <rmi@mastercore.us>
2024-04-05 09:00:33 -0400

    [FIX]#19118 unique_vat_by_partner

--- unique_vat_by_partner/models/univat.py
+++ unique_vat_by_partner/models/univat.py
@@ -8,7 +8,7 @@
 ###############################################################################
 
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import ValidationError, UserError
 
 class UniVat(models.Model):
     _inherit = 'res.partner'
@@ -16,24 +16,42 @@ class UniVat(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         recs = super(UniVat, self).create(vals_list)
-
-        for rec in recs:
-            if rec.vat:
-                same_vat = self.env['res.partner'].search([
-                    ('vat', '=', rec.vat),
-                    ('id', '!=', rec.id),
-                    ('l10n_latam_identification_type_id', '=',
-                        rec.l10n_latam_identification_type_id.id),
-                ])
-
-                if same_vat:
-                    child = []
-                    if rec.child_ids:
-                        child = [p.id for p in rec.child_ids]
-                    if rec.parent_id:
-                        child.append(rec.parent_id.id)
-                    if same_vat.id not in child:
-                        raise ValidationError(
-                            _('Ya se encuentra registrado el Número de Identificación %s para el Contacto (%s)') % (rec.vat, same_vat.name))
-
+        self._validate_duplicate_vat(recs)
         return recs
+
+    def write(self, vals):
+        res = super(UniVat, self).write(vals)
+        if 'vat' in vals:
+            self._validate_duplicate_vat(self)
+        return res
+
+    def _validate_duplicate_vat(self, partners):
+        if not isinstance(partners, models.BaseModel):
+            for partner in partners:
+                self._validate_single_duplicate_vat(partner)
+        else:
+            self._validate_single_duplicate_vat(partners)
+
+    def _validate_single_duplicate_vat(self, partner):
+        for rec in partner:
+            if rec.vat:
+                same_vats = self.env['res.partner'].search([
+                    ('vat', '=', rec.vat),
+                    ('parent_id','=',False),
+                    ('l10n_latam_identification_type_id', '=', rec.l10n_latam_identification_type_id.id),
+                ])
+                if len(same_vats)>1:
+                    raise ValidationError(
+                                _('Ya se encuentra registrado el Número de Identificación %s para el Contacto (%s)') % (rec.vat, same_vats[0].name))
+
+    
+    @api.constrains('vat')
+    def _constrains_validate_vat(self):
+        for rec in self:
+            validate = True
+            if rec.vat:
+                validate = rec.vat.isdigit()
+            else:
+                raise UserError(_('El Número de Identificación es requerido'))
+            if not validate:
+                raise UserError(_('El Número de Identificación %s sólo debe contener números') % (rec.vat))
