99af2e58b5b7bb2049cea80ed7faaa416fdccd71
Rouse Milano <rmi@mastercore.us>
2024-04-29 18:36:46 -0400

    [FIX] #19575 unique_vat_by_partner vat_type_partner

--- vat_type_partner/models/res_partner.py
+++ vat_type_partner/models/res_partner.py
@@ -7,53 +7,20 @@
 #
 ###############################################################################
 
-from odoo import models, fields, api, _
+from odoo import models, api, _
 from odoo.exceptions import ValidationError
 
-class UniVat(models.Model):
+class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        rec = super(UniVat, self).create(vals_list)
-        for val in vals_list:
-            if val.get('l10n_latam_identification_type_id'):
-                company_type = val.get('company_type')
-                identification_type_id = self.env['l10n_latam.identification.type']\
-                    .browse(int(val.get('l10n_latam_identification_type_id')))
-                if identification_type_id.l10n_ve_code in ['J','G','C'] and company_type == 'person':
-                    raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-                
-                if identification_type_id.l10n_ve_code in ['V','P','E'] and company_type == 'company':
-                    raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-                    
-        return rec
-
-    def write(self, vals):
-        res = super(UniVat, self).write(vals)
-        if 'l10n_latam_identification_type_id' in vals:
-            identification_type_id = self.env['l10n_latam.identification.type']\
-                    .browse(int(vals.get('l10n_latam_identification_type_id')))
-            if identification_type_id.l10n_ve_code in ['J','G','C'] and self.company_type == 'person':
-                    raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-                
-            if identification_type_id.l10n_ve_code in ['V','P','E'] and self.company_type == 'company':
-                raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-            
-        elif 'company_type' in vals:
-            if self.l10n_latam_identification_type_id.l10n_ve_code in ['J','G','C'] and vals.get('company_type') == 'person':
-                    raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-                
-            if self.l10n_latam_identification_type_id.l10n_ve_code in ['V','P','E'] and vals.get('company_type') == 'company':
-                raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-            
-        elif 'l10n_latam_identification_type_id' in vals and 'company_type' in vals:
-            identification_type_id = self.env['l10n_latam.identification.type']\
-                    .browse(int(vals.get('l10n_latam_identification_type_id')))
-            if identification_type_id.l10n_ve_code in ['J','G','C'] and vals.get('company_type') == 'person':
-                    raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-                
-            if identification_type_id.l10n_ve_code in ['V','P','E'] and vals.get('company_type') == 'company':
-                raise ValidationError('El tipo de identificación no corresponde con el tipo de compañia')
-
-        return res
+    @api.constrains('l10n_latam_identification_type_id','company_type')
+    def _constrains_validate_document_type(self):
+        document_type_person =['V', 'P', 'E']
+        document_type_company =['J', 'G', 'C']
+        for record in self:
+            if record.company_type == 'person' and len(record.parent_id)==0:
+                if record.l10n_latam_identification_type_id.l10n_ve_code not in document_type_person:
+                    raise ValidationError('El tipo de identificación no corresponde con la compañía tipo persona')
+            elif record.company_type == 'company':
+                if record.l10n_latam_identification_type_id.l10n_ve_code not in document_type_company:
+                    raise ValidationError('El tipo de identificación no corresponde con la compañía tipo compañía')
