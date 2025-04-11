7334dc17ed8d859a300f4fd2cb825606d7ebb869
Elvis Paez <elvispaez18@gmail.com>
2025-04-11 16:33:56 -0400

    [ADD] copy false in control number

--- l10n_ve_withholding/models/account_move.py
+++ l10n_ve_withholding/models/account_move.py
@@ -20,13 +20,13 @@ class AccountMove(models.Model):
         'Control Number', size=80,
         help="Number used to manage pre-printed invoices, by law you will"
              " need to put here this number to be able to declarate on"
-             " Fiscal reports correctly.",store=True)
+             " Fiscal reports correctly.",store=True, copy=False)
     applied_withholding_tax = fields.Boolean(
         'Retencion de IVA aplicada', compute='_compute_applied_withholding',
-        store=True, copy=False, default=False)
+        store=True, copy=False, default=False, copy=False)
     applied_withholding_islr = fields.Boolean(
         'Retencion de ISLR aplicada', compute='_compute_applied_withholding',
-        store=True, copy=False, default=False)
+        store=True, copy=False, default=False, copy=False)
 
 
     @api.depends('amount_residual','amount_residual_signed',)
