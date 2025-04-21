37535666fd2957185ce9568c45bd24890e202a02
Elvis Paez <elvispaez18@gmail.com>
2025-04-21 12:01:09 -0400

    [ADD] withholding

--- l10n_ve_withholding/__manifest__.py
+++ l10n_ve_withholding/__manifest__.py
@@ -17,7 +17,7 @@
 
     'author': "SINAPSYS GLOBAL SA || MASTERCORE SAS",
     'website': "http://sinapsys.global",
-    'version': '16.0.0.1.6',
+    'version': '16.0.0.1.7',
     'category': 'Localization',
     'license': 'AGPL-3',
     'depends': [
