046a17e8588512b2a79e258cfb2c453fe1a3690e
José Bracho <brachojgbp@gmail.com>
2025-06-07 17:03:20 -0400

    [IMP] #odoo/my-tasks/2052

---
+++ l10n_ve_account/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Author: SINAPSYS GLOBAL SA || MASTERCORE SAS
+# Copyleft: 2020-Present.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#
+#
+###############################################################################
+{
+    'name': "Venezuelan Accounting",
+    'description': "",
+    'author': "SINAPSYS GLOBAL SA || MASTERCORE SAS",
+    'website': "http://sinapsys.global",
+    'version': '16.0.1.0.1',
+    'category': 'Localization',
+    'countries': ['ve'],
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'contacts',
+        'l10n_ve_base'
+    ],
+    'data': [
+        'views/account_move.xml'
+    ]
+}
