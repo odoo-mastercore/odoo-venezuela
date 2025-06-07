e7739f2a4cd698beb27906dd1cfeb697f8163651
José Bracho <brachojgbp@gmail.com>
2025-06-07 14:19:03 -0400

    [IMP] #odoo/my-tasks/2050

--- l10n_ve_base/models/res_partner.py
+++ l10n_ve_base/models/res_partner.py
@@ -15,9 +15,9 @@ class ResPartner(models.Model):
 
     country_id = fields.Many2one(
         'res.country',
-        string=u'País',
+        string='País',
         ondelete='restrict',
-        help=u"País",
+        help="País",
         default=lambda self: self.env['res.country'].search(
             [('name', '=', 'Venezuela')]
         )[0].id
@@ -26,62 +26,32 @@ class ResPartner(models.Model):
         "res.country.state",
         string='Estado',
         ondelete='restrict',
-        help=u"Estado"
+        help="Estado"
     )
     municipality_id = fields.Many2one(
         "res.country.state.municipality",
         string="Municipio",
         domain="[('state_id', '=', state_id)]",
         ondelete='restrict',
-        help=u"Municipio"
+        help="Municipio"
     )
     parish_id = fields.Many2one(
         "res.country.state.municipality.parish",
         string="Parroquia",
         ondelete='restrict',
         domain="[('municipality_id', '=', municipality_id)]",
-        help=u"Parroquia"
+        help="Parroquia"
     )
     l10n_latam_identification_type_id = fields.Many2one(
         'l10n_latam.identification.type', string="Identification Type",
         index=True, auto_join=True,
-        # default=lambda self: self.env.ref('l10n_ve_base.it_civ'),
+        default=lambda self: self.env.ref('l10n_ve_base.it_civ') or False,
         help="The type of identification")
     l10n_ve_responsibility_type_id = fields.Many2one(
         'l10n_ve.responsibility.type', string='SENIAT Responsibility Type',
         index=True, help='Defined by SENIAT to identify the type of '
         'responsibilities that a person or a legal entity could have and that '
         'impacts in the type of operations and requirements they need.')
-
-    # @api.onchange('state_id')
-    # def _onchange_state_id(self):
-    #     if self.state_id:
-    #         return {
-    #             'value': {
-    #                 'municipality_id': '',
-    #                 'parish_id': ''
-    #             },
-    #             'domain': {
-    #                 'municipality_id': [
-    #                     ('state_id', '=', self.state_id.id)
-    #                 ]
-    #             },
-    #         }
-    #     else:
-    #         return {'domain': {'state_id': []}}
-
-    # @api.onchange('municipality_id')
-    # def _onchange_municipality_id(self):
-    #     if self.municipality_id:
-    #         return {'value': {'parish_id': ''}}
-
-    # @api.onchange('country_id')
-    # def _onchange_country(self):
-    #     return {
-    #         'value': {
-    #             'l10n_ve_responsibility_type_id': ''
-    #         },
-    #     }
 
     @api.constrains('vat', 'l10n_latam_identification_type_id')
     def check_vat(self):
