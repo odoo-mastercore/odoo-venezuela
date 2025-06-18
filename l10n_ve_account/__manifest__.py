5e0eaa584e80fba5b6ab2098321332ec70c52b48
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2025-06-18 12:06:56 -0400

    Update __manifest__.py

--- l10n_ve_account/__manifest__.py
+++ l10n_ve_account/__manifest__.py
@@ -5,6 +5,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 #
 #
+#
 ###############################################################################
 {
     'name': "Venezuelan Accounting",
