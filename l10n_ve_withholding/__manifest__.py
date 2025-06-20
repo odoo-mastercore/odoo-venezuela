3e0e0cce6ffb886780090f965a054abdbe126ccd
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2025-06-20 07:27:37 -0400

    Update __manifest__.py

--- l10n_ve_withholding/__manifest__.py
+++ l10n_ve_withholding/__manifest__.py
@@ -3,7 +3,6 @@
 # Copyleft: 2023-Present.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 #
-#
 ###############################################################################
 {
     'name': "Localización Withholding Venezuela",
