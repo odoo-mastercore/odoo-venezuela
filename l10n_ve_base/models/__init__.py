6054ecbc725f0877a682ecf5b29a97cb45622b65
Mastercore Sinapsys Global® <33432708+odoo-mastercore@users.noreply.github.com>
2025-08-29 10:57:16 -0400

    [UPD] res_company

--- l10n_ve_base/models/__init__.py
+++ l10n_ve_base/models/__init__.py
@@ -11,5 +11,5 @@ from . import l10n_ve_responsibility_type
 from . import res_partner
 from . import res_bank
 from . import res_partner_bank
-from . import res_company
+#from . import res_company
 from . import res_currency_rate
