d944f0041a5c718184ff23ff71427f31715ed227
José Bracho <brachojgbp@gmail.com>
2025-09-01 14:35:18 -0400

    [IMP] l10n_ve_base: default raise in identification_type

--- l10n_ve_base/models/res_company.py
+++ l10n_ve_base/models/res_company.py
@@ -44,7 +44,7 @@ class ResCompany(models.Model):
     l10n_latam_identification_type_id = fields.Many2one(
         'l10n_latam.identification.type', string="Identification Type",
         index=True, auto_join=True,
-        default=lambda self: self.env.ref('l10n_ve_base.it_rifj'),
+        default=lambda self: self.env.ref('l10n_ve_base.it_rifj', raise_if_not_found=False),
         help="The type of identification")
     l10n_ve_responsibility_type_id = fields.Many2one(
         'l10n_ve.responsibility.type', string='SENIAT Responsibility Type',
