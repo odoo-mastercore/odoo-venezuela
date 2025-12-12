13a6b1f461d9fd8e303d3526f295e00f7f91b20f
Elvis Paez <elvispaez18@gmail.com>
2025-12-12 14:09:33 -0400

    [REF] super _prepare_default_reversal

--- l10n_ve_withholding/wizard/account_move_reverse.py
+++ l10n_ve_withholding/wizard/account_move_reverse.py
@@ -15,21 +15,12 @@ class AccountMoveReversal(models.TransientModel):
 
 
     def _prepare_default_reversal(self, move):
-        reverse_date = self.date if self.date_mode == 'custom' else move.date
-        return {
-            'ref': _('Reversal of: %(move_name)s, %(reason)s', move_name=move.name, reason=self.reason)
-                   if self.reason
-                   else _('Reversal of: %s', move.name),
-            'date': reverse_date,
-            'invoice_date_due': reverse_date,
-            'invoice_date': move.is_invoice(include_receipts=True) and (self.date or move.date) or False,
-            'journal_id': self.journal_id.id,
-            'invoice_payment_term_id': None,
-            'invoice_user_id': move.invoice_user_id.id,
-            'auto_post': 'at_date' if reverse_date > fields.Date.context_today(self) else 'no',
+        reverse_vals = super(AccountMoveReversal, self)._prepare_default_reversal(move)
+        reverse_vals.update({
             'l10n_ve_document_number': "",
-            'invoice_origin': move.ref
-        }
+            'invoice_origin': move.ref,
+        })
+        return reverse_vals
 
 
     #TODO: ver si esto es necesario.
