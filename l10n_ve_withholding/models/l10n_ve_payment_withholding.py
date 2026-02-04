c7987bc28fbd2229aa5eeaeb888a0a8d81960a56
Elvis Paez <elvispaez18@gmail.com>
2026-02-04 11:05:59 -0400

    [ADD] withholding total

--- l10n_ve_withholding/models/l10n_ve_payment_withholding.py
+++ l10n_ve_withholding/models/l10n_ve_payment_withholding.py
@@ -274,8 +274,14 @@ class l10nVePaymentWithholding(models.Model):
 
     def _get_withholding_lines(self):
         lines = []
+        total_base = 0.0
+        total_iva = 0.0
+        total_retenido = 0.0
         if self.l10n_ve_move_line_taxes_ids:
             for idx, tax in enumerate(self.l10n_ve_move_line_taxes_ids, start=1):
+                total_base += tax.tax_base_amount
+                total_iva += tax.debit if tax.move_id.move_type == 'in_invoice' else tax.credit
+                total_retenido += tax.debit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100 if tax.debit else tax.credit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100
                 lines.append({
                     'index': idx,
                     'date': tax.move_id.invoice_date.strftime('%d-%m-%Y'),
@@ -286,14 +292,26 @@ class l10nVePaymentWithholding(models.Model):
                     'reserved_entry': True if tax.move_id.reversed_entry_id else False,
                     'reserved_entry_ref': tax.move_id.reversed_entry_id.ref or '',
                     'amount_total': self.payment_id._format_miles_number(round(abs(tax.move_id.amount_total_signed), 2)),
-                    'amount_untaxed': self.payment_id._format_miles_number(tax.move_id.get_exempt_amount()),
-                    'tax_base_amount': self.payment_id._format_miles_number(tax.tax_base_amount),
+                    'amount_untaxed_formated': self.payment_id._format_miles_number(tax.move_id.get_exempt_amount()),
+                    'amount_untaxed': tax.move_id.get_exempt_amount(),
+                    'tax_base_amount_formated': self.payment_id._format_miles_number(tax.tax_base_amount),
+                    'tax_base_amount': tax.tax_base_amount,
                     'tax_name': tax.tax_line_id.name,
                     'partner_vat_retention': str(self._get_partner_alicuot(self.payment_id.partner_id)),
-                    'tax_debit': self.payment_id._format_miles_number(tax.debit) if tax.move_id.move_type == 'in_invoice' else self.payment_id._format_miles_number(tax.credit),
-                    'tax_amount': self.payment_id._format_miles_number(tax.debit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100) if tax.debit else self.payment_id._format_miles_number(tax.credit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100),
+                    'tax_debit_formated': self.payment_id._format_miles_number(tax.debit) if tax.move_id.move_type == 'in_invoice' else self.payment_id._format_miles_number(tax.credit),
+                    'tax_debit': tax.debit if tax.move_id.move_type == 'in_invoice' else tax.credit,
+                    'tax_amount_formated': self.payment_id._format_miles_number(tax.debit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100) if tax.debit else self.payment_id._format_miles_number(tax.credit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100),
+                    'tax_amount': tax.debit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100 if tax.debit else tax.credit * int(self._get_partner_alicuot(self.payment_id.partner_id)) / 100,
                 })
-        return lines
+        body = {
+            'lines': lines,
+            'total': {
+                'total_base': self.payment_id._format_miles_number(round(total_base, 2)),
+                'total_iva': self.payment_id._format_miles_number(round(total_iva, 2)),
+                'total_retenido': self.payment_id._format_miles_number(round(total_retenido, 2)),
+            }
+        }
+        return body
 
     def _get_sustraendo(self):
         if self.l10n_ve_regimen_islr_id:
