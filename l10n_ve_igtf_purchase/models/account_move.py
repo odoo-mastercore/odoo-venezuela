513a2134a07ec5a13d1d1834c127edfe10420989
Elvis Paez <elvispaez18@gmail.com>
2026-07-10 16:37:27 -0400

    [FIX] l10n_ve_igtf_purchase: preserve configured IGTF on invoice posting

--- l10n_ve_igtf_purchase/models/account_move.py
+++ l10n_ve_igtf_purchase/models/account_move.py
@@ -7,6 +7,7 @@
 #
 ###############################################################################
 from odoo import models, fields, api
+from odoo.tools import formatLang
 
 
 class AccountMove(models.Model):
@@ -28,10 +29,16 @@ class AccountMove(models.Model):
         'invoice_payment_term_id',
         'partner_id',
         'currency_id',
-        'igtf_purchase_apply_purchase'
+        'igtf_purchase_apply_purchase',
+        'igtf_base_purchase',
+        'igtf_amount_purchase',
+        'igtf_base_purchase_usd',
+        'igtf_amount_purchase_usd',
     )
     def _compute_tax_totals(self):
         super(AccountMove, self)._compute_tax_totals()
+        for move in self:
+            move._inject_igtf_purchase_tax_totals()
 
     def _get_igtf_purchase_tax(self):
         self.ensure_one()
@@ -74,6 +81,70 @@ class AccountMove(models.Model):
             'tax_repartition_line_id': repartition_line.id,
             'tax_tag_ids': repartition_line.tag_ids.ids,
         }
+
+    def _inject_igtf_purchase_tax_totals(self):
+        self.ensure_one()
+        if not self.tax_totals or not self.is_invoice(include_receipts=True):
+            return
+
+        tax = self._get_igtf_purchase_tax()
+        igtf_tax_values = self._get_igtf_purchase_tax_values()
+        if not tax or not tax.tax_group_id or not igtf_tax_values:
+            return
+
+        tax_totals = dict(self.tax_totals)
+        groups_by_subtotal = dict(tax_totals.get('groups_by_subtotal') or {})
+        subtotal_name = next(
+            (
+                subtotal.get('name')
+                for subtotal in tax_totals.get('subtotals', [])
+                if subtotal.get('name') in groups_by_subtotal
+            ),
+            False,
+        ) or next(iter(groups_by_subtotal), False)
+        if not subtotal_name:
+            return
+
+        subtotal_groups = list(groups_by_subtotal.get(subtotal_name, []))
+        amount = self.currency_id.round(abs(igtf_tax_values['amount_currency'] or igtf_tax_values['balance']))
+        base_amount = self.currency_id.round(abs(
+            self.igtf_base_purchase_usd
+            if self.currency_id != self.company_currency_id
+            else self.igtf_base_purchase
+        ))
+
+        igtf_group = {
+            'group_key': tax.tax_group_id.id,
+            'tax_group_id': tax.tax_group_id.id,
+            'tax_group_name': tax.tax_group_id.name,
+            'tax_group_amount': amount,
+            'tax_group_base_amount': base_amount,
+            'formatted_tax_group_amount': formatLang(self.env, amount, currency_obj=self.currency_id),
+            'formatted_tax_group_base_amount': formatLang(self.env, base_amount, currency_obj=self.currency_id),
+            'hide_base_amount': False,
+        }
+
+        existing_group = next(
+            (
+                group for group in subtotal_groups
+                if group.get('tax_group_id') == tax.tax_group_id.id
+            ),
+            False,
+        )
+        existing_amount = existing_group.get('tax_group_amount', 0.0) if existing_group else 0.0
+        if existing_group:
+            existing_group.update(igtf_group)
+        else:
+            subtotal_groups.append(igtf_group)
+
+        groups_by_subtotal[subtotal_name] = subtotal_groups
+        tax_totals['groups_by_subtotal'] = groups_by_subtotal
+
+        computed_total = (tax_totals.get('amount_total') or 0.0) + (amount - existing_amount)
+        total_amount = self.currency_id.round(max(self.amount_total, computed_total))
+        tax_totals['amount_total'] = total_amount
+        tax_totals['formatted_amount_total'] = formatLang(self.env, total_amount, currency_obj=self.currency_id)
+        self.tax_totals = tax_totals
 
     def _cleanup_legacy_igtf_purchase_lines(self):
         legacy_lines = self.line_ids.filtered(
